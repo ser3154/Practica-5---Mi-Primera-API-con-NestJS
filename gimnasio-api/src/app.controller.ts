01ece53ebe674aed9b7fdd5fbd48a2558d648d36
ser3154 <servandocj45@outlook.es>
2026-09-08 19:24:51 -0700

    Terminado los pasos

--- gimnasio-api/src/app.controller.ts
+++ gimnasio-api/src/app.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
-
+interface Clase {
+  id: number;
+  nombre: string;
+}
 
 @Controller()
 export class AppController {
+  private clases: Clase[] = [
+    { id: 1, nombre: 'Yoga' },
+    { id: 2, nombre: 'Spinning' },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('clases')
+  getClases(): Clase[] {
+    return this.clases;
+  }
+
+  @Post('clases')
+  crearClase(@Body() nuevaClase: Clase): Clase {
+    this.clases.push(nuevaClase);
+    return nuevaClase;
   }
 }
